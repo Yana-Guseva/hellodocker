6ced34f8b9e17c6ce055c8f3e2d0b5889bffcf90
Yana <9301YanaGuseva@gmail.com>
2019-04-28 15:14:34 +0300

    fix

--- src/main/java/HelloDocker.java
+++ src/main/java/HelloDocker.java
@@ -1,6 +1,5 @@
 public class HelloDocker {
-    //
     public static void main(String[] args) {
-        System.out.println("Hello Docker!");
+        System.out.println("Hello Docker!!!");
     }
 }
